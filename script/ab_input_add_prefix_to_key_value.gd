ed208abc5e0afd7f3fc9174aabaaecb3345f17fe
Eloi Stree <eloistree@gmail.com>
2026-09-08 23:06:17 +0200

    Save

--- script/ab_input_add_prefix_to_key_value.gd
+++ script/ab_input_add_prefix_to_key_value.gd
@@ -17,3 +17,13 @@ func push_in_key_value_boolean(key:String, boolean:bool):
 
 func push_in_key_value_text(key:String, text:String):
 	on_parsed_key_value_text.emit(_prefix_to_append + key + _suffix_to_append, text)
+
+
+func push_in_empty_key_value_analog( analog:float):
+	on_parsed_key_value_analog.emit(_prefix_to_append  + _suffix_to_append, analog)
+
+func push_in_empty_key_value_boolean( boolean:bool):
+	on_parsed_key_value_boolean.emit(_prefix_to_append  + _suffix_to_append, boolean)
+
+func push_in_empt_key_value_text( text:String):
+	on_parsed_key_value_text.emit(_prefix_to_append + _suffix_to_append, text)
